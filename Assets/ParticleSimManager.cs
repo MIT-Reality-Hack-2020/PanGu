179a5453d41895917b90d877120dd661e64ceb38
Rustin Wollin <rwollin96@gmail.com>
2020-01-17 13:04:28 -0500

    Initial commit

---
+++ Assets/ParticleSimManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
+
+public class ParticleSimManager : MonoBehaviour {
+    public ComputeShader particleSimShader;
+    public Material particleDebugMaterial;
+    public Mesh particleMesh;
+
+    public uint particleCount = 1000;
+
+    private ComputeBuffer drawArgs;
+    private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+
+    private ComputeBuffer particleBufferA;
+    private ComputeBuffer particleBufferB;
+    private bool pingPong;
+    
+    private int          verletIndex;
+    private int densityPressureIndex;
+    private int           forceIndex;
+    
+    struct GPUParticle {
+        public float3 position;
+        public float  density;
+        public float3 velocity;
+        public float  pressure;
+        public float3 force;
+        //public float  temperature;
+        public uint   type; // 0 = earth, 1 = water, 2 = fire, 3 = wood, 4 = metal
+    }
+
+    private void Start() {
+        particleBufferA = new ComputeBuffer(256 * 256 * 256, sizeof(float) * 12);
+        particleBufferB = new ComputeBuffer(256 * 256 * 256, sizeof(float) * 12);
+        
+                 verletIndex = particleSimShader.FindKernel("Verlet");
+        densityPressureIndex = particleSimShader.FindKernel("DensityPressure");
+                  forceIndex = particleSimShader.FindKernel("Force");
+                  
+        drawArgs = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        args[0] = particleMesh.GetIndexCount(0);
+        args[2] = particleMesh.GetIndexStart(0);
+        args[3] = particleMesh.GetBaseVertex(0);
+                                                                      
+        var initialParticles = new GPUParticle[particleCount];
+        var rand = new Random(283927);
+        for (int i = 0; i < particleCount; i++) {
+            initialParticles[i] = new GPUParticle();
+            initialParticles[i].position = rand.NextFloat3(new float3(-10f, -10f, -10f), new float3(10f, 10f, 10f));
+            initialParticles[i].velocity = rand.NextFloat3(new float3(-10f, -10f, -10f), new float3(10f, 10f, 10f));
+            //Debug.Log($"{initialParticles[i].position} {initialParticles[i].velocity}");
+        }
+        
+        particleBufferA.SetData(initialParticles);
+    }
+
+    private void PingPongBuffers() {
+        particleSimShader.SetBuffer(verletIndex, "lastParticles", pingPong ? particleBufferA : particleBufferB);
+        particleSimShader.SetBuffer(verletIndex, "nextParticles", pingPong ? particleBufferB : particleBufferA);
+
+        particleSimShader.SetBuffer(densityPressureIndex, "lastParticles", pingPong ? particleBufferA : particleBufferB);
+        particleSimShader.SetBuffer(densityPressureIndex, "nextParticles", pingPong ? particleBufferB : particleBufferA);
+
+        particleSimShader.SetBuffer(forceIndex, "lastParticles", pingPong ? particleBufferA : particleBufferB);
+        particleSimShader.SetBuffer(forceIndex, "nextParticles", pingPong ? particleBufferB : particleBufferA);
+        
+        pingPong = !pingPong;
+    }
+
+    private void Update() {
+        PingPongBuffers();
+        //particleSimShader.Dispatch(verletIndex, Mathf.CeilToInt(particleCount / 128f), 1, 1);
+    }
+
+    private void OnDestroy() {
+        particleBufferA.Release();
+        particleBufferB.Release();
+    }
+
+    private void LateUpdate() {
+        args[1] = particleCount;
+        drawArgs.SetData(args);
+        
+        particleDebugMaterial.SetPass(0);
+        particleDebugMaterial.SetBuffer("_ParticleBuffer", particleBufferA);
+        
+        Graphics.DrawMeshInstancedIndirect(particleMesh, 0, particleDebugMaterial, new Bounds(Vector3.zero, Vector3.one * 1000f), drawArgs);
+    }
+}
---
+++ Assets/PlantManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Physics;
+using Unity.Mathematics;
+using Unity.Rendering;
+
+using Unity.Mathematics;
+using Collider = Unity.Physics.Collider;
+
+/*
+public class PlantManager : MonoBehaviour {
+    public GameObject stalkSegmentPrefab;
+    public int plantCount;
+    
+    private void Start() {
+        var entityManager = World.Active.EntityManager;
+        
+        Entity sourceEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(stalkSegmentPrefab, World.Active);
+        entityManager.AddComponent<PhysicsJoint>(sourceEntity);
+
+        BlobAssetReference<JointData> fixedJointData = JointData.CreateFixed();
+        BlobAssetReference<JointData> ballAndSocketData = JointData.CreateBallAndSocket(new float3(), new float3());
+        
+        
+        entityManager.AddComponentData<JointData>(sourceEntity, new JointData { });
+
+        BlobAssetReference<Collider> segmentCapsuleCollider =
+            entityManager.GetComponentData<PhysicsCollider>(sourceEntity).Value;
+
+        for (int i = 0; i < plantCount; i++) {
+            var baseSegment = entityManager.Instantiate(sourceEntity);
+            entityManager.SetComponentData(baseSegment, new Translation { Value = new float3() });
+            entityManager.SetComponentData(baseSegment, new Rotation { Value = quaternion.identity });
+            entityManager.SetComponentData(baseSegment, new PhysicsCollider { Value = segmentCapsuleCollider });
+            
+            for (int j = 0; j < i; j++) {
+                var nextSegment = entityManager.Instantiate(sourceEntity);
+                entityManager.SetComponentData(nextSegment, new Translation { Value = new float3() });
+                entityManager.SetComponentData(nextSegment, new Rotation { Value = quaternion.identity });
+                entityManager.SetComponentData(nextSegment, new PhysicsCollider { Value = segmentCapsuleCollider });
+            }
+        }
+    }
+}
+*/
