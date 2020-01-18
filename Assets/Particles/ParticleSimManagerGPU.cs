abd71cf7cef0294a046f99451a67baad86e5f95a
Rustin Wollin <rwollin96@gmail.com>
2020-01-18 18:14:33 -0500

    start on hybrid SPH method, neighbor lists are built on CPU and transfered to GPU

--- Assets/Particles/ParticleSimManagerGPU.cs
+++ Assets/Particles/ParticleSimManagerGPU.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 using Unity.Mathematics;
 using UnityEngine.UI;
@@ -25,10 +28,14 @@ public class ParticleSimManagerGPU : MonoBehaviour {
 
     private ComputeBuffer drawArgs;
     private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    
+    private NativeList<GPUParticle> particleList;
 
     private ComputeBuffer particleBufferA;
-    private ComputeBuffer particleBufferB;
-    private bool pingPong;
+    //private bool pingPong;
+
+    private ComputeBuffer neighborInfoBuffer;
+    private ComputeBuffer neighborListBuffer;
     
     private int          verletIndex;
     private int densityPressureIndex;
@@ -48,13 +55,19 @@ public class ParticleSimManagerGPU : MonoBehaviour {
         public float  pad1;
     }
 
+    struct NeighborInfo {
+        public int startIndex;
+        public int count;
+    }
+
     public static ParticleSimManagerGPU instance;
 
     private void Awake() {
         instance = this;
         
-        particleBufferA = new ComputeBuffer(256 * 256 * 256, sizeof(float) * 16);
-        particleBufferB = new ComputeBuffer(256 * 256 * 256, sizeof(float) * 16);
+        particleBufferA = new ComputeBuffer(64 * 64 * 64, sizeof(float) * 16);
+        neighborInfoBuffer = new ComputeBuffer(64 * 64* 64, sizeof(int) * 2);
+        neighborListBuffer = new ComputeBuffer(64 * 64 * 64 * 32, sizeof(int));
         
                  verletIndex = particleSimShader.FindKernel("Verlet");
         densityPressureIndex = particleSimShader.FindKernel("DensityPressure");
@@ -89,21 +102,110 @@ public class ParticleSimManagerGPU : MonoBehaviour {
         particleBufferA.SetData(initialParticles);
         //particleBufferB.SetData(initialParticles);
     }
-
-    private void PingPongBuffers() {
-        pingPong = !pingPong;
+    
+    [BurstCompile]
+    private struct PopulateSpatialHashMapJob : IJobParallelFor {
+        [ReadOnly] public float cellSize;
+        [ReadOnly] public float boundsSize;
         
-        particleSimShader.SetBuffer(verletIndex, "lastParticles", pingPong ? particleBufferA : particleBufferB);
-        particleSimShader.SetBuffer(verletIndex, "nextParticles", pingPong ? particleBufferB : particleBufferA);
+        [ReadOnly] public NativeArray<GPUParticle> particles;
+        //[WriteOnly] public NativeMultiHashMap<int, int> particleSpatialHashMap;
+        [WriteOnly] public NativeMultiHashMap<int, int>.ParallelWriter particleSpatialHashMapWriter;
+        
+        public void Execute(int i) {
+            //var parallelHashMap = particleSpatialHashMap.AsParallelWriter();
+            int hash = (int)math.hash(new int3(math.floor((particles[i].position/* - boundsSize*/) / cellSize)));
+            particleSpatialHashMapWriter.Add(hash, i);
+        }
+    }
 
-        particleSimShader.SetBuffer(densityPressureIndex, "lastParticles", pingPong ? particleBufferA : particleBufferB);
-        particleSimShader.SetBuffer(densityPressureIndex, "nextParticles", pingPong ? particleBufferB : particleBufferA);
+    [BurstCompile]
+    private struct PopulateNeighborListArrays : IJobParallelFor {
+        [ReadOnly] public float cellSize;
+        [ReadOnly] public NativeArray<GPUParticle> particles;
+        [ReadOnly] public NativeMultiHashMap<int, int> particleSpatialHashMap;
+        [WriteOnly, NativeDisableParallelForRestriction] public NativeArray<int> neighborList;
+        [WriteOnly] public NativeArray<NeighborInfo> neighborInfo;
 
-        particleSimShader.SetBuffer(forceIndex, "lastParticles", pingPong ? particleBufferA : particleBufferB);
-        particleSimShader.SetBuffer(forceIndex, "nextParticles", pingPong ? particleBufferB : particleBufferA);
+        public void Execute(int i) {
+            GPUParticle thisParticle = particles[i];
+            int3 thisBucket = new int3(math.floor((particles[i].position /* - boundsSize*/) / cellSize));
+
+            int neighbors = 0;
+            
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        int hash = (int) math.hash(thisBucket + new int3(x, y, z));
+                        if (particleSpatialHashMap.ContainsKey(hash)) {
+                            var enumerator = particleSpatialHashMap.GetValuesForKey(hash);
+                            do {
+                                if (neighbors < 32) {
+                                    neighborList[i * 32 + neighbors] = enumerator.Current;
+                                    neighbors++;
+                                }
+                            } while (enumerator.MoveNext());
+                        }
+                    }
+                }
+            }
+
+            neighborInfo[i] = new NeighborInfo { startIndex = i * 32, count = neighbors};
+        }
     }
 
     private void Update() {
+        // copy particles back from GPU -- slow, but not enough time to implement every part of non-brute-force SPH on GPU
+        particleList = new NativeList<GPUParticle>((int)particleCount, Allocator.TempJob);
+        var particleManaged = new GPUParticle[particleCount];
+        particleBufferA.GetData(particleManaged, 0, 0, (int)particleCount);
+        for (int i = 0; i < particleCount; i++) {
+            particleList.Add(particleManaged[i]);
+        }
+        
+        // loop over particles and make pending deletions, etc
+        // TODO
+        
+        // construct spatial acceleration structure
+        var particleSpatialHashMap = new NativeMultiHashMap<int, int>(particleList.Length, Allocator.TempJob);
+        var neighborInfoNativeArray = new NativeArray<NeighborInfo>(particleList.Length, Allocator.TempJob);
+        var neighborListNativeArray = new NativeArray<int>(particleList.Length * 32, Allocator.TempJob);
+        
+        var populateSpatialHashMapJob = new PopulateSpatialHashMapJob();
+        populateSpatialHashMapJob.particleSpatialHashMapWriter = particleSpatialHashMap.AsParallelWriter();
+        populateSpatialHashMapJob.boundsSize = 1f;
+        populateSpatialHashMapJob.cellSize = smoothingLength;
+        populateSpatialHashMapJob.particles = particleList;
+
+        var updateSpatialHashMapHandle = populateSpatialHashMapJob.Schedule(particleList.Length, 64);
+        
+        var populateNeighborListArraysJob = new PopulateNeighborListArrays();
+        populateNeighborListArraysJob.particles = particleList;
+        populateNeighborListArraysJob.cellSize = smoothingLength;
+        populateNeighborListArraysJob.neighborInfo = neighborInfoNativeArray;
+        populateNeighborListArraysJob.neighborList = neighborListNativeArray;
+        populateNeighborListArraysJob.particleSpatialHashMap = particleSpatialHashMap;
+
+        var populateNeighborListArraysHandle = populateNeighborListArraysJob.Schedule(particleList.Length, 64, updateSpatialHashMapHandle);
+        
+        populateNeighborListArraysHandle.Complete();
+        particleSpatialHashMap.Dispose();
+        
+        neighborInfoBuffer.SetData(neighborInfoNativeArray);
+        neighborListBuffer.SetData(neighborListNativeArray);
+        
+        neighborInfoNativeArray.Dispose();
+        neighborListNativeArray.Dispose();
+        particleList.Dispose();
+
+        particleSimShader.SetBuffer(verletIndex, "particles", neighborInfoBuffer);
+        particleSimShader.SetBuffer(densityPressureIndex, "particles", neighborInfoBuffer);
+        particleSimShader.SetBuffer(forceIndex, "particles", neighborInfoBuffer);
+        
+        particleSimShader.SetBuffer(verletIndex, "particles", neighborListBuffer);
+        particleSimShader.SetBuffer(densityPressureIndex, "particles", neighborListBuffer);
+        particleSimShader.SetBuffer(forceIndex, "particles", neighborListBuffer);
+        
         //Debug.Log($"{particleCount}");
         particleSimShader.SetInt("particleCount", (int)particleCount);
         
@@ -130,7 +232,6 @@ public class ParticleSimManagerGPU : MonoBehaviour {
 
     private void OnDestroy() {
         particleBufferA.Release();
-        particleBufferB.Release();
     }
 
     private void LateUpdate() {
@@ -142,6 +243,10 @@ public class ParticleSimManagerGPU : MonoBehaviour {
         particleDebugMaterial.SetBuffer("_ParticleBuffer", particleBufferA);
         
         Graphics.DrawMeshInstancedIndirect(particleMesh, 0, particleDebugMaterial, new Bounds(Vector3.zero, Vector3.one * 1000f), drawArgs);
+
+        //var particlesManaged = new GPUParticle[particleCount];
+        //particleBufferA.GetData(particlesManaged);
+        //particleList = ;
     }
 
     public void SpawnParticle(float3 position, float3 velocity, HandController.Element element) {
